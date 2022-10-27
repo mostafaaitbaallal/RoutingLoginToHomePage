a732cef720e01a814599c38be8b58c628a262534
mostafaaitbaallal <mostafa.aitbaallal@gmail.com>
2022-10-28 00:08:18 +0200

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './layouts/auth/auth.component';
-import { MainComponent } from './views/main/main.component';
+import { ForgotPasswordComponent } from './views/shared/forgot-password/forgot-password.component';
+import { NotFoundComponent } from './views/shared/not-found/not-found.component';
+import { ListStudentComponent } from './views/student/list-student/list-student.component';
+import { AddStudentComponent } from './views/student/add-student/add-student.component';
+import { UpdateStudentComponent } from './views/student/update-student/update-student.component';
+import { DeleteStudentComponent } from './views/student/delete-student/delete-student.component';
+import { LoginComponent } from './views/auth/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent,
-    MainComponent
+    ForgotPasswordComponent,
+    NotFoundComponent,
+    ListStudentComponent,
+    AddStudentComponent,
+    UpdateStudentComponent,
+    DeleteStudentComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
