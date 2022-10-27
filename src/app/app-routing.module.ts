142194e47b98fea4f3f7bb3c5b87cc813f244782
mostafaaitbaallal <mostafa.aitbaallal@gmail.com>
2022-10-28 00:08:45 +0200

    Add Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,50 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// layouts
+import { MainComponent } from "./layouts/main/main.component";
+import { AuthComponent } from "./layouts/auth/auth.component";
+
+// admin views
+import { AddStudentComponent } from "./views/student/add-student/add-student.component";
+import { ListStudentComponent } from "./views/student/list-student/list-student.component";
+
+
+// auth views
+import { LoginComponent } from "./views/auth/login/login.component";
+
+
+// views without layouts
+
+import { ForgotPasswordComponent } from './views/auth/forgot-password/forgot-password.component';
+import { NotFoundComponent } from './views/shared/not-found/not-found.component';
+
+const routes: Routes = [
+
+  {
+    path: "main",
+    component: MainComponent,
+    children: [
+      { path: "ListStudent", component: ListStudentComponent },
+      { path: "AddStudent", component: AddStudentComponent },
+      { path: "", redirectTo: "listStudent", pathMatch: "full" },
+    ],
+  },
+  {
+    path: "Auth",
+    component: AuthComponent,
+    children: [
+      { path: "Login", component: LoginComponent },
+      { path: "ForgotPassword", component: ForgotPasswordComponent },
+    ],
+  },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
