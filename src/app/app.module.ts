3c935128d28021e1d412058de256eeb7f5f8b4b8
mostafaaitbaallal <mostafa.aitbaallal@gmail.com>
2022-10-27 16:29:31 +0200

    Adding auth and main layouts componenents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './layouts/auth/auth.component';
+import { MainComponent } from './views/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
